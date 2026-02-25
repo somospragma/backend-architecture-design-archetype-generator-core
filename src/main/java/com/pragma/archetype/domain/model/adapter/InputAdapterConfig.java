e899ba0bf516505484e2ee64e2b037aed80e9c66
david.yepes <david.yepesb@pragma.com.co>
2026-02-25 09:47:57 -0500

    Task 0.13: Replace manual Builder with @Builder in InputAdapterConfig

--- src/main/java/com/pragma/archetype/domain/model/adapter/InputAdapterConfig.java
+++ src/main/java/com/pragma/archetype/domain/model/adapter/InputAdapterConfig.java
@@ -2,60 +2,17 @@ package com.pragma.archetype.domain.model.adapter;
 
 import java.util.List;
 
+import lombok.Builder;
+
 /**
  * Configuration for generating an input adapter (REST, GraphQL, etc.).
  * Represents the input needed to generate a complete input adapter.
  */
+@Builder
 public record InputAdapterConfig(
     String name,
     String packageName,
     InputAdapterType type,
     String useCaseName,
     List<Endpoint> endpoints) {
-
-  public static Builder builder() {
-    return new Builder();
-  }
-
-  public static class Builder {
-    private String name;
-    private String packageName;
-    private InputAdapterType type;
-    private String useCaseName;
-    private List<Endpoint> endpoints;
-
-    public Builder name(String name) {
-      this.name = name;
-      return this;
-    }
-
-    public Builder packageName(String packageName) {
-      this.packageName = packageName;
-      return this;
-    }
-
-    public Builder type(InputAdapterType type) {
-      this.type = type;
-      return this;
-    }
-
-    public Builder useCaseName(String useCaseName) {
-      this.useCaseName = useCaseName;
-      return this;
-    }
-
-    public Builder endpoints(List<Endpoint> endpoints) {
-      this.endpoints = endpoints;
-      return this;
-    }
-
-    public InputAdapterConfig build() {
-      return new InputAdapterConfig(
-          name,
-          packageName,
-          type,
-          useCaseName,
-          endpoints);
-    }
-  }
 }
