433bf1de8f5d6558d47c7f5b4fa82da7f1aa6b2a
david.yepes <david.yepesb@pragma.com.co>
2026-02-25 09:50:31 -0500

    Task 0.16: Replace manual Builder with @Builder in UseCaseConfig

--- src/main/java/com/pragma/archetype/domain/model/usecase/UseCaseConfig.java
+++ src/main/java/com/pragma/archetype/domain/model/usecase/UseCaseConfig.java
@@ -2,11 +2,14 @@ package com.pragma.archetype.domain.model.usecase;
 
 import java.util.List;
 
+import lombok.Builder;
+
 /**
  * Configuration for generating a use case.
  * Represents the input needed to generate a complete use case with its port and
  * implementation.
  */
+@Builder
 public record UseCaseConfig(
     String name,
     String packageName,
@@ -29,51 +32,5 @@ public record UseCaseConfig(
   public record MethodParameter(
       String name,
       String type) {
-  }
-
-  public static Builder builder() {
-    return new Builder();
-  }
-
-  public static class Builder {
-    private String name;
-    private String packageName;
-    private List<UseCaseMethod> methods;
-    private boolean generatePort = true;
-    private boolean generateImpl = true;
-
-    public Builder name(String name) {
-      this.name = name;
-      return this;
-    }
-
-    public Builder packageName(String packageName) {
-      this.packageName = packageName;
-      return this;
-    }
-
-    public Builder methods(List<UseCaseMethod> methods) {
-      this.methods = methods;
-      return this;
-    }
-
-    public Builder generatePort(boolean generatePort) {
-      this.generatePort = generatePort;
-      return this;
-    }
-
-    public Builder generateImpl(boolean generateImpl) {
-      this.generateImpl = generateImpl;
-      return this;
-    }
-
-    public UseCaseConfig build() {
-      return new UseCaseConfig(
-          name,
-          packageName,
-          methods,
-          generatePort,
-          generateImpl);
-    }
   }
 }
